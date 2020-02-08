e46a70bd6bfa820f005b1d45a4a47155eabfe5a6
Squidable <zhangphil@outlook.com>
2020-02-08 09:54:46 -0800

    Add files via upload

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+   public int health;
+   public GameObject deathEffect;
+    private void Update() {
+        if (health <= 0) {
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+   public void TakeDamage(int damage) {
+       health -= damage;
+   }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float offset;
+
+    public GameObject projectile;
+    public Transform shotPoint;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (timeBtwShots <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+       else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
